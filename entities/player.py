f6a662077941c3bf431447b6873b8e94e8a1f976
fran-00 <fran.mf011@gmail.com>
2023-09-04 21:30:09 +0200

    Change character_face_direction variable name to facing_direction in update_animation method

--- entities/player.py
+++ entities/player.py
@@ -1,6 +1,7 @@
 import os
 
 import arcade
+from .entity import Entity
 from shared_constants import *
 
 
@@ -26,15 +27,15 @@ class PlayerCharacter(Entity):
 
     def update_animation(self, delta_time: float = 1/60):
         # Figure out if we need to flip face left or right
-        if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
-            self.character_face_direction = LEFT_FACING
-        elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
-            self.character_face_direction = RIGHT_FACING
+        if self.change_x < 0 and self.facing_direction == RIGHT_FACING:
+            self.facing_direction = LEFT_FACING
+        elif self.change_x > 0 and self.facing_direction == LEFT_FACING:
+            self.facing_direction = RIGHT_FACING
 
         if mouse_left_pressed == False:
             # Idle animation
             if self.change_x == 0 and self.change_y == 0 or UPDATES_PER_FRAME == 0:
-                self.texture = self.idle_texture_pair[self.character_face_direction]
+                self.texture = self.idle_texture_pair[self.facing_direction]
                 return
 
             else:
@@ -42,7 +43,7 @@ class PlayerCharacter(Entity):
                 if self.cur_texture > 7 * UPDATES_PER_FRAME:
                     self.cur_texture = 0
                 frame = self.cur_texture // UPDATES_PER_FRAME
-                direction = self.character_face_direction
+                direction = self.facing_direction
 
                 # Walking to SOUTH
                 if self.change_x == 0 and self.change_y < 0:
@@ -58,5 +59,5 @@ class PlayerCharacter(Entity):
 
         elif mouse_left_pressed == True:
             # FIXME Fighting animation
-            self.texture = self.fight_texture_pair[self.character_face_direction]
+            self.texture = self.fight_texture_pair[self.facing_direction]
             return
