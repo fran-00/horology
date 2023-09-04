0d9ec6b36dcb4ddd5bec17973b9baf9bff93b255
fran-00 <fran.mf011@gmail.com>
2023-09-04 21:28:54 +0200

    Update PlayerCharacter: from now on loading sprites logic to load the sprites will be handled by Entity parent class

--- entities/player.py
+++ entities/player.py
@@ -15,48 +15,14 @@ def load_texture_pair(filename):
     ]
 
 
-class PlayerCharacter(arcade.Sprite):
-    def __init__(self):             # TODO add max_health as argument
+class PlayerCharacter(Entity):
+    """Player Sprite"""
+
+    def __init__(self):
+
         # Set up parent class
-        super().__init__()
+        super().__init__("player", "player")
 
-        # Default to face-right
-        self.character_face_direction = RIGHT_FACING
-
-        # Used for flipping between image sequences
-        self.cur_texture = 0
-
-        self.scale = SPRITE_SCALING
-
-        # Adjust the collision box. Box is centered at sprite center, (0, 0)
-        # Quadranti:      III          IV         I          II
-        self.points = [[-22, -52], [22, -52], [22, 28], [-22, 28]]
-
-        # --- Load Textures ---
-        main_path = os.path.join(os.getcwd(), "sprite_pack/pg")
-
-        # Load textures for idle and fighting standing
-        self.idle_texture_pair = load_texture_pair(
-            f"{main_path}_idlefront.png")
-
-        self.fight_texture_pair = load_texture_pair(
-            f"{main_path}_fight_front.png")
-
-        # Load textures for walking
-        self.walk_textures = []
-        for i in range(8):
-            texture = load_texture_pair(f"{main_path}_walk{i}.png")
-            self.walk_textures.append(texture)
-
-        self.walkfront_textures = []
-        for i in range(8):
-            texture = load_texture_pair(f"{main_path}_walkfront{i}.png")
-            self.walkfront_textures.append(texture)
-
-        self.walkback_textures = []
-        for i in range(8):
-            texture = load_texture_pair(f"{main_path}_walkback{i}.png")
-            self.walkback_textures.append(texture)
 
     def update_animation(self, delta_time: float = 1/60):
         # Figure out if we need to flip face left or right
