5b29699b36b048d6be7db84c9c0c4c1e27bc3a49
fran-00 <fran.mf011@gmail.com>
2023-09-04 21:57:18 +0200

    Add Enemy class which inherits from Entity class

--- entities/enemies.py
+++ entities/enemies.py
@@ -1,5 +1,40 @@
 import math
-import arcade
+
+from .entity import Entity
+from shared_constants import *
+
+
+class Enemy(Entity):
+    def __init__(self, name_folder, name_file):
+
+        # Setup parent class
+        super().__init__(name_folder, name_file)
+
+        self.should_update_walk = 0
+
+    def update_animation(self, delta_time: float = 1 / 60):
+
+        # Figure out if we need to flip face left or right
+        if self.change_x < 0 and self.facing_direction == RIGHT_FACING:
+            self.facing_direction = LEFT_FACING
+        elif self.change_x > 0 and self.facing_direction == LEFT_FACING:
+            self.facing_direction = RIGHT_FACING
+
+        # Idle animation
+        if self.change_x == 0:
+            self.texture = self.idle_texture_pair[self.facing_direction]
+            return
+
+        # Walking animation
+        if self.should_update_walk == 3:
+            self.cur_texture += 1
+            if self.cur_texture > 7:
+                self.cur_texture = 0
+            self.texture = self.walk_textures[self.cur_texture][self.facing_direction]
+            self.should_update_walk = 0
+            return
+
+        self.should_update_walk += 1
 
 
 def follow_sprite(self, player_sprite):
