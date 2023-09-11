1e298ba63cc4154f59928eac75d3e893e37c5895
fran-00 <fran.mf011@gmail.com>
2023-09-11 10:37:44 +0200

    Move create_bullet method into PlayerCharacter class

--- core/game.py
+++ core/game.py
@@ -96,9 +96,7 @@ class GameView(arcade.View):
         # we check if left button is pressed to change player sprite to fighting version
         if button == arcade.MOUSE_BUTTON_LEFT:
             self.player_sprite.mouse_left_pressed = True
-            
-            bullet = self.create_bullet(x, y)
-            # Add the bullet to the appropriate lists
+            bullet = self.player_sprite.create_bullet(self, x, y)
             self.bullet_list.append(bullet)
     
     def move_on_mouse_click(self, x, y):
@@ -120,51 +118,6 @@ class GameView(arcade.View):
             self.player_sprite.change_x = 0
             self.player_sprite.change_y = 0
             return
-
-    def create_bullet(self, x, y):
-        """Spawn a bullet that travels from player to target"""
-        # Create a bullet
-        bullet = arcade.Sprite(
-            "sprite_pack/4dEuclideanCube.png", SPRITE_SCALING_CURSE)
-
-        # Position the bullet at the player's current location
-        start_x = self.player_sprite.center_x
-        start_y = self.player_sprite.center_y
-        bullet.center_x = start_x
-        bullet.center_y = start_y
-        # TODO il proiettile viene fuori dalla mano e cambia mano in base all'orientamento della sprite di lei
-
-        # Get from the mouse the destination location for the bullet
-        # right now target's coordinates system origin is precisely the
-        # bottom left angle of the viewport. Viewport follows the player..
-        # IMPORTANT! If you have a scrolling screen, you will also need to
-        # add in self.view_bottom and self.view_left. But HOW?
-        target_x = x + self.view_left
-        target_y = y + self.view_bottom
-
-        # Calculate how to get the bullet to the destination: the angle in
-        # radians between the start points and end points is the one the
-        # bullet will travel: 2-argument arctangent is equal the angle
-        # between the positive x axis and the ray to the point (x, y) ≠ (0, 0)
-        # In our case coordinates x and y is the difference between player
-        # position and the point aimed with the mouse cursor in our
-        # coordinates system where 0.0 is in the bottom left of the viewport
-
-        x_diff = target_x - start_x
-        y_diff = target_y - start_y
-        angle = math.atan2(y_diff, x_diff)
-
-        # Angle the bullet sprite so it doesn't look like it is flying sideways.
-        bullet.angle = math.degrees(angle)
-        print(f"Bullet angle: {bullet.angle:.2f}")
-
-        print(f"target_x = {target_x}, target_y = {target_y}")
-        # Taking into account the angle, calculate our change_x and change_y.
-        # Velocity is how fast the bullet travels.
-        bullet.change_x = math.cos(angle) * BULLET_SPEED
-        bullet.change_y = math.sin(angle) * BULLET_SPEED
-
-        return bullet
 
     def on_mouse_release(self, x, y, button, modifiers):
         """Handle mouse button release"""
