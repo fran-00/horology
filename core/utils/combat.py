5e7c11a8cfb56498debd713b373c966b10e36288
fran-00 <fran.mf011@gmail.com>
2024-01-07 15:29:40 +0100

    Add damage_enemy_with_ranged_attack method

--- core/utils/combat.py
+++ core/utils/combat.py
@@ -94,16 +94,20 @@ class Combat:
             wall_hit_list = arcade.check_for_collision_with_list(
                 bullet, self.game_view.scene[LAYER_NAME_WALLS])
             # If it did, get rid of the bullet
-            # if len(enemy_hit_list) > 0:
-            #     bullet.remove_from_sprite_lists()
+            if len(enemy_hit_list) > 0:
+                bullet.remove_from_sprite_lists()
 
-            # # remove enemy tile if a bullet hit him
-            # for enemy in enemy_hit_list:
-            #     # Enemy.hp -= stuff.dmg
-            #     # if Enemy.hp <= 0:
-            #     enemy.remove_from_sprite_lists()
+            self.damage_enemy_with_ranged_attack(enemy_hit_list)
 
             # Remove bullet if it hits an obstacle which is not an enemy:
             for _ in wall_hit_list:
                 bullet.remove_from_sprite_lists()
             # Now bullet will travel forever and will go out of screen
+
+    def damage_enemy_with_ranged_attack(self, enemy_hit_list):
+        for enemy in enemy_hit_list:
+            enemy.hp -= self.game_view.player.equipped_weapon.damage
+            if enemy.hp <= 0:
+                enemy.remove_from_sprite_lists()
+            else:
+                print(f"Enemy has {enemy.hp} remaining.")
