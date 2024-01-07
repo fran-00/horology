cc90c8ba90ab457065f27e8b8c70fdd15131b6cf
fran-00 <fran.mf011@gmail.com>
2024-01-07 12:41:46 +0100

    Add spawn_enemies and get_damage_from_enemy to EnemyAI

--- core/enemy_ai.py
+++ core/enemy_ai.py
@@ -1,5 +1,6 @@
 import arcade
 
+from entities.enemies import EnemyCharacter
 from shared_constants import *
 
 
@@ -23,3 +24,41 @@ class EnemyAI:
             bullet = enemy.shoot_at_player(delta_time)
             if bullet:
                 self.game_view.scene[LAYER_NAME_BULLETS].append(bullet)
+
+    def spawn_enemies(self):
+        """Spawn an enemy when a spawn point is triggered"""
+        spawn_points_touched_list = arcade.check_for_collision_with_list(self.game_view.player,
+                                                                         self.game_view.scene[LAYER_NAME_SPAWN_TRIGGER])
+        if spawn_points_touched_list != []:
+            for spawn_point in spawn_points_touched_list:
+                enemy_name = spawn_point.properties["name"]
+                enemy_hp = spawn_point.properties["hp"]
+                enemy_damage = spawn_point.properties["damage"]
+                # remove the spawn point triggered from sprite list
+                spawn_point.kill()
+                print("Prepare to fight! Spawn point touched!")
+
+            enemy = EnemyCharacter(enemy_name, enemy_hp, enemy_damage, self.game_view.player, self.game_view.scene[LAYER_NAME_WALLS])
+            # Position the enemy 100 pixels away horizontally
+            enemy.center_x = spawn_point.center_x + 100
+            enemy.center_y = spawn_point.center_y
+
+            # Add the enemy to the lists spawning it at a random location
+            self.game_view.scene[LAYER_NAME_ENEMIES].append(enemy)
+
+    def get_damage_from_enemy(self):
+        """Handle fights with enemies"""
+        enemies_hit_list = arcade.check_for_collision_with_list(self.game_view.player,
+                                                                self.game_view.scene[LAYER_NAME_ENEMIES])
+        # If player touch an ENEMY, she loses as many hp as is written on damage property
+        if self.game_view.player.cur_health > 0:
+            for enemy in enemies_hit_list:
+                hp_lost = int(enemy.damage)
+                self.game_view.player.cur_health -= hp_lost
+        # If player's health reaches 0, she respawns at starting coordinates with full health (for now)
+        else:
+            self.game_view.player.change_x = 0
+            self.game_view.player.change_y = 0
+            self.game_view.player.center_x = PLAYER_START_X
+            self.game_view.player.center_y = PLAYER_START_Y
+            self.game_view.player.cur_health = self.game_view.player.max_health
