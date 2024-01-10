6783ecad8b83c4fb1c69935f33eb1fb53e1d77fb
fran-00 <fran.mf011@gmail.com>
2024-01-10 13:21:48 +0100

    Move main function inside launcher

--- launcher.py
+++ launcher.py
@@ -1,4 +1,14 @@
-from game.gui.main_menu import main
+import arcade
+
+from game.gui.main_menu import MainMenuView
+from shared_constants import *
+
+
+def main():
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    menu_view = MainMenuView()
+    window.show_view(menu_view)
+    arcade.run()
 
 if __name__ == "__main__":
     main()
