0fc575671666280ddde26a653252392cd4e07f91
fran-00 <fran.mf011@gmail.com>
2024-01-12 16:10:09 +0100

    Replace MenuView with StartView on game startup

--- launcher.py
+++ launcher.py
@@ -1,12 +1,18 @@
 import arcade
 
-from game.gui.main_menu import MainMenuView
+from game.gui.start_view import StartView
 from game.constants import Constants as c
+
+
+class MyWindow(arcade.Window):
+    def __init__(self):
+        super().__init__(c.SCREEN_WIDTH, c.SCREEN_HEIGHT, c.SCREEN_TITLE, resizable=True)
+        self.views = {}
 
 
 def main():
     window = arcade.Window(c.SCREEN_WIDTH, c.SCREEN_HEIGHT, c.SCREEN_TITLE)
-    menu_view = MainMenuView()
+    menu_view = StartView()
     window.show_view(menu_view)
     arcade.run()
 
