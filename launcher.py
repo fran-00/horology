da55718bcea82cbc37597abd066c88001fa59c2c
fran-00 <fran.mf011@gmail.com>
2024-01-12 16:17:01 +0100

    Change game window class name to GameWindow and update main function to call it

--- launcher.py
+++ launcher.py
@@ -4,14 +4,14 @@ from game.gui.start_view import StartView
 from game.constants import Constants as c
 
 
-class MyWindow(arcade.Window):
+class GameWindow(arcade.Window):
     def __init__(self):
         super().__init__(c.SCREEN_WIDTH, c.SCREEN_HEIGHT, c.SCREEN_TITLE, resizable=True)
         self.views = {}
 
 
 def main():
-    window = arcade.Window(c.SCREEN_WIDTH, c.SCREEN_HEIGHT, c.SCREEN_TITLE)
+    window = GameWindow()
     menu_view = StartView()
     window.show_view(menu_view)
     arcade.run()
