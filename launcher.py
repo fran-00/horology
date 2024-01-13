a55ddf02bb4269d355a7b031313dcf09697ff1dc
fran-00 <fran.mf011@gmail.com>
2024-01-13 11:50:32 +0100

    Add GameView to GameWindow views

--- launcher.py
+++ launcher.py
@@ -1,6 +1,7 @@
 import arcade
 
 from game.views.start_view import StartView
+from game.core.game_view import GameView
 from game.views.main_menu_view import MainMenuView
 from game.constants import Constants as c
 
@@ -9,6 +10,7 @@ class GameWindow(arcade.Window):
     def __init__(self):
         super().__init__(c.SCREEN_WIDTH, c.SCREEN_HEIGHT, c.SCREEN_TITLE, resizable=True)
         self.views = {}
+        self.views["game"] = GameView()
         self.views["main_menu"] = MainMenuView()
 
 
