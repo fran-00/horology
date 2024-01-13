2a6496e995f7ffba8af75035e1ebb050660fe3d5
fran-00 <fran.mf011@gmail.com>
2024-01-13 15:48:42 +0100

    Update Hud class indentation

--- game/ui/hud.py
+++ game/ui/hud.py
@@ -11,31 +11,37 @@ class Hud:
         """Render player HP number"""
         # Draw how many hit points we have
         health_string = f"{self.game_view.player.cur_health}/{self.game_view.player.max_health}"
-        arcade.draw_text(health_string,
-                         start_x=self.game_view.view_left + 750,
-                         start_y=self.game_view.view_bottom + 0,
-                         font_size=25,
-                         font_name="Kenney Pixel",
-                         color=arcade.color.WHITE)
+        arcade.draw_text(
+                health_string,
+                start_x=self.game_view.view_left + 750,
+                start_y=self.game_view.view_bottom + 0,
+                font_size=25,
+                font_name="Kenney Pixel",
+                color=arcade.color.WHITE
+        )
 
     def draw_health_bar(self):
         """Render player HP bar"""
         # Draw the red background of the bar
         if self.game_view.player.cur_health < self.game_view.player.max_health:
-            arcade.draw_rectangle_filled(center_x=self.game_view.view_left + 600,
-                                         center_y=self.game_view.view_bottom + 10,
-                                         width=c.HEALTHBAR_WIDTH,
-                                         height=10,
-                                         color=arcade.color.RED)
+            arcade.draw_rectangle_filled(
+                    center_x=self.game_view.view_left + 600,
+                    center_y=self.game_view.view_bottom + 10,
+                    width=c.HEALTHBAR_WIDTH,
+                    height=10,
+                    color=arcade.color.RED
+            )
 
         # Calculate width based on health
         health_width = c.HEALTHBAR_WIDTH * (self.game_view.player.cur_health / self.game_view.player.max_health)
         # Draw the green foreground of the bar
-        arcade.draw_rectangle_filled(center_x=(self.game_view.view_left + 600) - 0.5 * (c.HEALTHBAR_WIDTH - health_width),
-                                     center_y=self.game_view.view_bottom - 10,
-                                     width=health_width,
-                                     height=c.HEALTHBAR_HEIGHT,
-                                     color=arcade.color.GREEN)
+        arcade.draw_rectangle_filled(
+                center_x=(self.game_view.view_left + 600) - 0.5 * (c.HEALTHBAR_WIDTH - health_width),
+                center_y=self.game_view.view_bottom - 10,
+                width=health_width,
+                height=c.HEALTHBAR_HEIGHT,
+                color=arcade.color.GREEN
+        )
 
     def load_hotbar_sprites(self):
         """Load the sprites for the hotbar at the bottom of the screen.
@@ -48,12 +54,13 @@ class Hud:
         first_number_pad_sprite_index = 51
         last_number_pad_sprite_index = 61
 
-        self.game_view.hotbar_sprite_list = arcade.load_spritesheet(file_name="resources/tilesets/input_prompts.png",
-                                                                    sprite_width=16,
-                                                                    sprite_height=16,
-                                                                    columns=34,
-                                                                    count=816,
-                                                                    margin=1,
+        self.game_view.hotbar_sprite_list = arcade.load_spritesheet(
+                file_name="resources/tilesets/input_prompts.png",
+                sprite_width=16,
+                sprite_height=16,
+                columns=34,
+                count=816,
+                margin=1,
         )[first_number_pad_sprite_index:last_number_pad_sprite_index]
 
     def draw_inventory(self):
@@ -64,21 +71,21 @@ class Hud:
         field_width = self.game_view.window.width / capacity
 
         arcade.draw_xywh_rectangle_filled(
-            bottom_left_x=self.game_view.view_left,
-            bottom_left_y=self.game_view.view_bottom,
-            width=self.game_view.window.width,
-            height=hotbar_height,
-            color=arcade.color.CHARCOAL
+                bottom_left_x=self.game_view.view_left,
+                bottom_left_y=self.game_view.view_bottom,
+                width=self.game_view.window.width,
+                height=hotbar_height,
+                color=arcade.color.CHARCOAL
         )
         for i in range(capacity):
             if i == self.game_view.selected_item - 1:
                 arcade.draw_xywh_rectangle_outline(
-                    bottom_left_x=(i * field_width) + self.game_view.view_left,
-                    bottom_left_y=self.game_view.view_bottom,
-                    width=self.game_view.window.width / capacity,
-                    height=hotbar_height,
-                    color=arcade.color.WHITE,
-                    border_width =2
+                        bottom_left_x=(i * field_width) + self.game_view.view_left,
+                        bottom_left_y=self.game_view.view_bottom,
+                        width=self.game_view.window.width / capacity,
+                        height=hotbar_height,
+                        color=arcade.color.WHITE,
+                        border_width =2
                 )
 
             if len(self.game_view.player.inventory) > i:
@@ -87,15 +94,18 @@ class Hud:
                 item_name = ""
 
             hotkey_sprite = self.game_view.hotbar_sprite_list[i]
-            hotkey_sprite.draw_scaled(center_x=((i * field_width) + self.game_view.view_left) + sprite_height / 2,
-                                      center_y=self.game_view.view_bottom + 20,
-                                      scale=2.0)
+            hotkey_sprite.draw_scaled(
+                    center_x=((i * field_width) + self.game_view.view_left) + sprite_height / 2,
+                    center_y=self.game_view.view_bottom + 20,
+                    scale=2.0
+            )
             # Add whitespace so the item text doesn't hide behind the number pad sprite
             text = f"      {item_name}"
-            arcade.draw_text(text,
-                             ((i * field_width) + self.game_view.view_left) + sprite_height / 2,
-                    self.game_view.view_bottom + 12,
-                    arcade.color.WHITE,
-                    25,
+            arcade.draw_text(
+                    text=text,
+                    start_x=((i * field_width) + self.game_view.view_left) + sprite_height / 2,
+                    start_y=self.game_view.view_bottom + 12,
+                    color=arcade.color.WHITE,
+                    font_size=25,
                     font_name="Kenney Pixel"
             )
