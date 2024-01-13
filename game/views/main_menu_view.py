e82517e2f616c3d3ddcec84f1e78e668af9449ae
fran-00 <fran.mf011@gmail.com>
2024-01-13 13:05:46 +0100

    Update buttons style using ButtonStyle class

--- game/views/main_menu_view.py
+++ game/views/main_menu_view.py
@@ -2,6 +2,7 @@ import arcade
 import arcade.gui
 
 from ..core.setup import setup
+from ..ui.buttons import ButtonStyle
 
 
 class MainMenuView(arcade.View):
@@ -17,19 +18,19 @@ class MainMenuView(arcade.View):
         # Create a vertical BoxGroup to align buttons
         self.v_box = arcade.gui.UIBoxLayout()
 
-        resume_button = arcade.gui.UIFlatButton(text="Resume Game", width=200)
+        resume_button = arcade.gui.UIFlatButton(text="Resume Game", width=200, style=ButtonStyle().default_style)
         self.v_box.add(resume_button.with_space_around(bottom=20))
         resume_button.on_click = self.on_click_resume
 
-        settings_button = arcade.gui.UIFlatButton(text="Settings", width=200)
+        settings_button = arcade.gui.UIFlatButton(text="Settings", width=200, style=ButtonStyle().default_style)
         self.v_box.add(settings_button.with_space_around(bottom=20))
         settings_button.on_click = self.on_click_settings
 
-        new_game_button = arcade.gui.UIFlatButton(text="New Game", width=200)
+        new_game_button = arcade.gui.UIFlatButton(text="New Game", width=200, style=ButtonStyle().default_style)
         self.v_box.add(new_game_button.with_space_around(bottom=20))
         new_game_button.on_click = self.on_click_new_game
 
-        quit_button = arcade.gui.UIFlatButton(text="Quit", width=200)
+        quit_button = arcade.gui.UIFlatButton(text="Quit", width=200, style=ButtonStyle().default_style)
         self.v_box.add(quit_button.with_space_around(bottom=20))
         quit_button.on_click = self.on_click_quit
         # Create a widget to hold the v_box widget, that will center the buttons
