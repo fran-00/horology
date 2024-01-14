fac984ed10cb0a9ca9595602bcb8cf9630264ee7
fran-00 <fran.mf011@gmail.com>
2024-01-14 17:54:49 +0100

    Update Bullet class to take bullets sprites from a tileset instead of a folder of png

--- game/entities/bullets.py
+++ game/entities/bullets.py
@@ -6,24 +6,23 @@ from ..constants import Constants as c
 class Bullet(arcade.Sprite):
     def __init__(self, name):
         super().__init__()
-
-        self.cur_texture = 0
+        self.cur_texture_index = 0
         self.scale = c.CHARACTER_SCALING / 2
 
-        main_path = f"resources/bullets/{name}/{name}"
-
-        self.bullet_textures = []
-        for i in range(7):
-            textures = arcade.load_texture(f"{main_path}_{i}.png")
-            self.bullet_textures.append(textures)
-
-        # Set the initial texture
-        self.texture = self.bullet_textures[0]
+        tileset_path = f"resources/bullets/{name}.png"
+        self.textures = arcade.load_spritesheet(
+            tileset_path,
+            sprite_width=c.SPRITE_SIZE,
+            sprite_height=c.SPRITE_SIZE,
+            columns=7,
+            count=7,
+        )
+        self.texture = self.textures[0]
         self.set_hit_box(self.texture.hit_box_points)
 
     def update_animation(self, delta_time: float = 1 / 60):
-        self.cur_texture += 1
-        if self.cur_texture > 6 * c.UPDATES_PER_FRAME:
-            self.cur_texture = 0
-        frame = self.cur_texture // c.UPDATES_PER_FRAME
-        self.texture = self.bullet_textures[frame]
+        self.cur_texture_index += 1
+        if self.cur_texture_index > 6:
+            self.cur_texture_index = 0
+        frame = self.cur_texture_index
+        self.texture = self.textures[frame]
