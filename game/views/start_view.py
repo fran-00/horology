bca5a4c8561afdebbce3a145cf60880555935637
fran-00 <fran.mf011@gmail.com>
2024-01-24 18:02:45 +0100

    Add add_buttons method to add new game, settings and quit buttons

--- game/views/start_view.py
+++ game/views/start_view.py
@@ -18,6 +18,28 @@ class StartView(arcade.View):
             )
         )
 
+    def add_buttons(self):
+        # NEW GAME button
+        new_game_button = arcade.gui.UIFlatButton(text="New Game",
+                                                  width=200,
+                                                  style=ButtonStyle().default_style)
+        self.v_box.add(new_game_button.with_space_around(bottom=20))
+        new_game_button.on_click = self.on_click_new_game
+
+        # SETTINGS button
+        settings_button = arcade.gui.UIFlatButton(text="Settings",
+                                                  width=200,
+                                                  style=ButtonStyle().default_style)
+        self.v_box.add(settings_button.with_space_around(bottom=20))
+        settings_button.on_click = self.on_click_settings
+
+        # QUIT button
+        quit_button = arcade.gui.UIFlatButton(text="Quit",
+                                              width=200,
+                                              style=ButtonStyle().default_style)
+        self.v_box.add(quit_button.with_space_around(bottom=20))
+        quit_button.on_click = self.on_click_quit
+
     def on_show_view(self):
         arcade.set_background_color(arcade.color.BLACK)
         arcade.set_viewport(0, self.window.width, 0, self.window.height)
