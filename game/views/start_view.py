0473f444bfeede2c7e132c65d2afc59f9b86c124
fran-00 <fran.mf011@gmail.com>
2024-01-24 18:13:11 +0100

    Update on_draw method of StartView class moving text on top of the screen

--- game/views/start_view.py
+++ game/views/start_view.py
@@ -49,12 +49,10 @@ class StartView(arcade.View):
     def on_draw(self):
         self.clear()
         self.manager.draw()
-        arcade.draw_text("Is this a GAME?", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2,
-                         arcade.color.WHITE, font_size=50, font_name="Kenney Pixel", anchor_x="center")
-        arcade.draw_text("Yes, it is!!!", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2 - 40,
-                         arcade.color.RED, font_size=20, font_name="Kenney Pixel", anchor_x="center")
-        arcade.draw_text("Click to start the game!", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2 - 70,
-                         arcade.color.GRAY, font_size=20, font_name="Kenney Pixel", anchor_x="center")
+        arcade.draw_text("Is this a GAME?", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT - 100,
+                         arcade.color.WHITE, font_size=70, font_name="Kenney Pixel", anchor_x="center")
+        arcade.draw_text("Yes, it is!!!", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT - 150,
+                         arcade.color.RED, font_size=50, font_name="Kenney Pixel", anchor_x="center")
 
     # Call back methods for buttons:
     def on_click_new_game(self, event):
