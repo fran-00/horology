2f95bd0280b0d8a151d26fb063c2d91d14b97cb9
fran-00 <fran.mf011@gmail.com>
2024-01-24 18:05:14 +0100

    Add call back methods for buttons

--- game/views/start_view.py
+++ game/views/start_view.py
@@ -55,3 +55,18 @@ class StartView(arcade.View):
                          arcade.color.RED, font_size=20, font_name="Kenney Pixel", anchor_x="center")
         arcade.draw_text("Click to start the game!", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2 - 70,
                          arcade.color.GRAY, font_size=20, font_name="Kenney Pixel", anchor_x="center")
+
+    # Call back methods for buttons:
+    def on_click_new_game(self, event):
+        self.window.show_view(self.window.views["game"])
+        setup(self.window.views["game"])
+
+    def on_click_settings(self, event):
+        self.window.show_view(self.window.views["settings"])
+
+    def on_click_quit(self, event):
+        self.window.close()
+
+    def on_key_press(self, key, _modifiers):
+        if key == arcade.key.ESCAPE:
+            self.window.close()
