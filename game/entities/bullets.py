07add9b8f4860b1063f2cafb1a879e6375b9f2de
fran-00 <fran.mf011@gmail.com>
2025-04-30 21:35:31 +0200

    Update Bullet class temporarily hiding texture assignment

--- game/entities/bullets.py
+++ game/entities/bullets.py
@@ -12,18 +12,13 @@ class Bullet(Sprite):
 
         tileset_path = f"resources/bullets/{name}.png"
         self.textures = load_spritesheet(
-            tileset_path,
-            sprite_width=c.SPRITE_SIZE,
-            sprite_height=c.SPRITE_SIZE,
-            columns=7,
-            count=7,
-            hit_box_algorithm="Simple",
+            tileset_path
         )
-        self.texture = self.textures[0]
+        # self.texture = self.textures
 
     def update_animation(self, delta_time: float = 1 / 60):
         self.cur_texture_index += 1
         if self.cur_texture_index > 6:
             self.cur_texture_index = 0
         frame = self.cur_texture_index
-        self.texture = self.textures[frame]
+        # self.texture = self.textures[frame]
